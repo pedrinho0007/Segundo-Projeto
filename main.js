c7858362eac6c49f4f65c5792b2523288b936479
pedrinho0007 <165796448+pedrinho0007@users.noreply.github.com>
2025-06-30 09:56:51 -0300

    cr7

--- main.js
+++ main.js
@@ -74,6 +74,6 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = entropia;
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
 
